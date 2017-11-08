f2eba58020a7300799b6c00f3abb6782e3b58a42
keshav-chaure <keshav.chaure@gmail.com>
2017-11-08 18:54:14 +0530

    added i18n support intl-react dependancy

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
-
+import {FormattedMessage} from 'react-intl';
+import {IntlProvider} from 'react-intl';
 class App extends React.Component {
+
+constructor(props) {
+        super(props);
+        this.state = {
+            name       : 'Eric',
+            unreadCount: 1000,
+        };
+    }
+
   render () {
-    return (
-      <div>
-        <p> Hello React Project</p>
-        <AwesomeComponent />
-      </div>
-	);
-  }
+        const {name, unreadCount} = this.state;
+
+        return (
+            <p>
+                <FormattedMessage
+                    id="welcome"
+                    defaultMessage={`Hello {name}, you have {unreadCount, number} {unreadCount, plural,
+                      one {message}
+                      other {messages}
+                    }`}
+                    values={{name: <b>{name}</b>, unreadCount}}
+                />
+            </p>
+        );
+        }
 }
 
-render(<App/>, document.getElementById('app'));
+render( <IntlProvider locale="en">
+        <App />
+    </IntlProvider>, document.getElementById('app'));
